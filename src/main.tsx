9fbdf9e8325e439df2edb03f44e679168493c609
Yin Chu <ycrijnaard@gmail.com>
2022-06-29 17:55:20 +0200

    Basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+// Framework imports
+
+// Dependency imports
+
+// Type + Interface imports
+
+// Context imports
+
+// Util imports
+
+// Component imports
+
+// Asset imports
+
+// Style imports
