ec11b0cf632e31ceccacbe3aed775df562f0fe6e
Yin Chu <ycrijnaard@gmail.com>
2022-07-01 01:33:14 +0200

    Non-working modal to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
 
 // Component imports
 import Entry from "./components/Entry";
+import EntryModal from "./components/EntryModal";
 
 const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -26,47 +27,9 @@ const App = () => {
         <i className="ri-add-fill ri-2x" onClick={onOpen}></i>
       </div>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <div className="mx-6 flex items-center justify-between p-1">
-            <Heading size="lg">Add countdown</Heading>
-            <i className="ri-close-fill ri-2x" onClick={onClose}></i>
-          </div>
-          <ModalBody>
-            <FormLabel htmlFor="title">Title</FormLabel>
-            <Input placeholder="Christmas 🎄" />
-
-            <br />
-            <br />
-
-            <FormLabel htmlFor="Date">Date</FormLabel>
-            <Input type="date" />
-
-            <br />
-            <br />
-
-            <FormLabel htmlFor="color">Color</FormLabel>
-            <Select placeholder="Select">
-              <option value="blue">Blue</option>
-              <option value="green">Green</option>
-              <option value="orange">Orange</option>
-              <option value="pink">Pink</option>
-              <option value="purple">Purple</option>
-              <option value="red">Red</option>
-              <option value="rose">Rose</option>
-              <option value="yellow">Yellow</option>
-            </Select>
-
-            <br />
-
-            <Button colorScheme="green" w="100%">
-              {/* <Text fontSize="lg">Add</Text> */}
-              <i className="ri-check-fill ri-lg"></i>
-            </Button>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      {isOpen ? (
+        <EntryModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+      ) : null}
 
       <br />
 
--- src/components/EntryModal.tsx
+++ src/components/EntryModal.tsx
@@ -7,28 +7,64 @@ import {
   useDisclosure,
   Heading,
   Input,
+  Select,
+  Text,
+  Button,
+  FormLabel,
 } from "@chakra-ui/react";
 
 interface EntryModalProps {
   isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
 const EntryModal = (props: EntryModalProps) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return null;
-  // <Modal isOpen={isOpen} onClose={onClose}>
-  //   <ModalOverlay />
-  //   <ModalContent>
-  //     <div className="flex items-center justify-between p-1">
-  //       <Heading size="lg">Add countdown</Heading>
-  //       <i className="ri-close-fill ri-2x" onClick={onClose}></i>
-  //     </div>
-  //     <ModalBody>
-  //       <Input placeholder="Basic usage" />
-  //     </ModalBody>
-  //   </ModalContent>
-  // </Modal>
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <div className="mx-6 flex items-center justify-between p-1">
+          <Heading size="lg">Add countdown</Heading>
+          <i className="ri-close-fill ri-2x" onClick={onClose}></i>
+        </div>
+        <ModalBody>
+          <FormLabel htmlFor="title">Title</FormLabel>
+          <Input placeholder="Christmas 🎄" />
+
+          <br />
+          <br />
+
+          <FormLabel htmlFor="Date">Date</FormLabel>
+          <Input type="date" />
+
+          <br />
+          <br />
+
+          <FormLabel htmlFor="color">Color</FormLabel>
+          <Select placeholder="Select">
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+            <option value="orange">Orange</option>
+            <option value="pink">Pink</option>
+            <option value="purple">Purple</option>
+            <option value="red">Red</option>
+            <option value="rose">Rose</option>
+            <option value="yellow">Yellow</option>
+          </Select>
+
+          <br />
+
+          <Button colorScheme="green" w="100%">
+            {/* <Text fontSize="lg">Add</Text> */}
+            <i className="ri-check-fill ri-lg"></i>
+          </Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default EntryModal;
